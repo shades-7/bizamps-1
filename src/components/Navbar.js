b5bea5cc71083331d57a4da95e439955a03b275b
Shweta <shweta3047@gmail.com>
2020-08-09 21:35:33 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,25 @@ import { Link,graphql } from "gatsby"
 import styles from "../stylesheets/navbar.module.scss"
 
 const Navbar = (props) => {
-    const toggleNav=()=>{
-      const nav=document.getElementById("nav")
-      if (nav.className === styles.nav) {
-        nav.className=styles.toggleNav
-      } else {
-        nav.className = styles.nav;
-      }
-    }
+    // const toggleNav=()=>{
+    //   const nav=document.getElementById("nav")
+    //   if (nav.className === styles.nav) {
+    //     nav.className=styles.toggleNav
+    //   } else {
+    //     nav.className = styles.nav;
+    //   }
+    // }
     return (
         <div className={styles.navContainer}>
           <div><Img fluid={props.logo} className={styles.logo}  /> </div>
-          <div className={styles.navToggle} onClick={toggleNav} ><span></span><span></span><span></span></div>
+          <div className={styles.navToggle}  ><span></span><span></span><span></span></div>
+          
           <ul className={styles.nav} id="nav">
-            <li className={styles.navlist}>Expertise
+            <li className={styles.navlist}>
+              <div style={{display:"flex",flexDirection:"row"}}>
+              <text className={styles.text}>Expertise</text>
+              <div className={styles.arrow} >{"<"}</div>
+              </div>
             <div className={styles.dropdown}>
                 <Link>[Guide] Best of ABM</Link>
                 <Link>[Guide] LeadGen Cold outreach</Link>
@@ -27,13 +32,21 @@ const Navbar = (props) => {
                 <Link>[Blog] Expectations for Outsourcing LeadGen?</Link>
             </div>   
             </li>
-            <li className={styles.navlist}>Pricing
+            <li className={styles.navlist}>
+              <div style={{display:"flex",flexDirection:"row"}}>
+            <text className={styles.text}>Pricing</text>
+              <div className={styles.arrow}>{"<"}</div>
+              </div>
             <div className={styles.dropdown}>
                 <Link>B2B Marketing Packages</Link>
                 <Link>Marketing Strategy Consultancy</Link>
             </div> 
             </li>
-            <li className={styles.navlist}>Case Stories
+            <li className={styles.navlist}>
+              <div style={{display:"flex",flexDirection:"row"}}>
+            <text className={styles.text}>Case Stories</text>
+              <div className={styles.arrow}>{"<"}</div>
+              </div>
             <div className={styles.dropdown}>
                 <Link>Indian Enterprises</Link>
                 <Link>International Institutes</Link>
