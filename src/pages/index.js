d2f485b8e1bdab43c4da53c8ad269a52c0e46af0
Shweta <shweta3047@gmail.com>
2020-08-11 20:17:13 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,9 +65,11 @@ const IndexPage = (props) => {
       /</BackgroundImage>
 
       <div className={styles.container2}>
+        <div className={styles.scrollContainer}>
         <IndexProcessCard title="THE DEMAND GENERATION PROCESS" subtitle="" content={process1} />
         <IndexProcessCard title="ACCOUNT BASED MARKETING PROCESS" subtitle="For Dream Accounts" content={process2} />
         <IndexProcessCard title="SALES ♡ MARKETING LEADS" subtitle="SQLs not MQLs" content={process3} />
+        </div>
       </div>
 
       <div className={styles.container3}>
